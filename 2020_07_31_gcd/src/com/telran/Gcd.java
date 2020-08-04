c8becb410a5c4106602522912d89adedaf41e18a
AnnaBo27 <anna.botvinovska@yahoo.de>
2020-08-04 21:30:27 +0200

    solution issue02 Greatest Common Divisor

---
+++ 2020_07_31_gcd/src/com/telran/Gcd.java
@@ -0,0 +1,24 @@
+package com.telran;
+
+public class Gcd {
+    /**
+     *Finds the greatest common divisor for two int positive numbers
+     *
+     * @param a >0
+     * @param b >0
+     * @return gcd of the two
+     *
+     *
+     */
+
+    public int compute(int a, int b){
+        if(b==0)
+            return a;
+        int d = a%b;
+        return compute(b,d);      //Wazno, tak kak mi ne znaem zna4alno, a>b ili net
+
+    }
+
+
+}
+
